7018214866cc9dda1e2a74d8f3fbf523c2022c14
matzz <matsubaray@eac.ne.jp>
2017-08-26 22:44:57 +0900

    type improvement

--- crawlla/crawlla/spiders/song.py
+++ crawlla/crawlla/spiders/song.py
@@ -10,23 +10,29 @@ class SongSpider(scrapy.Spider):
     start_urls = ('http://imascg-slstage-wiki.gamerch.com/',)
 
     def parse(self, response):
-        # yield scrapy.Request(response.css('#js_oc_box_m0 ul li a::attr("href")').extract_first(), self.parse_topics)
-         for url in response.css('#js_oc_box_m0 ul li a::attr("href")').extract():
-             yield scrapy.Request(url, self.parse_topics)
+        # yield scrapy.Request(response.css('#js_oc_box_m1 ul li a::attr("href")').extract_first(), self.parse_topics)
+        targets = ['#js_oc_box_m0','#js_oc_box_m1', '#js_oc_box_m2', '#js_oc_box_m3']
+        for target in targets:
+            for url in response.css(target).css('ul li a::attr("href")').extract():
+                yield scrapy.Request(url, self.parse_topics)
 
     def parse_topics(self, response):
+        typeOrg = response.xpath('//*[@id="js_async_main_column_text"]/div[1]/table[1]/tbody/tr/td[3]/span')
+        if len(typeOrg.xpath('a')) > 0 :
+            typeOrg = typeOrg.xpath('a/text()')
+        else:
+            typeOrg = typeOrg.xpath('text()')
         song = items.Song(
             title=response.xpath('//*[@id="js_async_main_column_name"]/text()').extract_first().strip(),
             bpm=response.xpath(
-                '//*[@id="js_async_main_column_text"]/div[1]/table/tbody/tr/td[1]/span/text()').extract_first(),
+                '//*[@id="js_async_main_column_text"]/div[1]/table[1]/tbody/tr/td[1]/span/text()').extract_first(),
             time=response.xpath(
-                '//*[@id="js_async_main_column_text"]/div[1]/table/tbody/tr/td[2]/span/text()').extract_first(),
-            type=response.xpath(
-                '//*[@id="js_async_main_column_text"]/div[1]/table/tbody/tr/td[3]/span/text()').extract_first(),
+                '//*[@id="js_async_main_column_text"]/div[1]/table[1]/tbody/tr/td[2]/span/text()').extract_first(),
+            type=typeOrg.extract_first(),
             category=response.xpath(
-                '//*[@id="js_async_main_column_text"]/div[1]/table/tbody/tr/td[4]/span/text()').extract_first(),
+                '//*[@id="js_async_main_column_text"]/div[1]/table[1]/tbody/tr/td[4]/span/text()').extract_first(),
             start=response.xpath(
-                '//*[@id="js_async_main_column_text"]/div[1]/table/tbody/tr/td[5]/span/text()').extract_first(),
+                '//*[@id="js_async_main_column_text"]/div[1]/table[1]/tbody/tr/td[5]/span/text()').extract_first(),
             difficulties=list(self.parse_difficulties(response))
         )
 
